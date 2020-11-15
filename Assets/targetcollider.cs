150dce6dd3773c30a764bcfda12534050e29f38c
FakhryanZ <fakhryanmuhammad@gmail.com>
2020-11-15 13:16:47 +0700

    finish praktikum

--- Assets/targetcollider.cs
+++ Assets/targetcollider.cs
@@ -6,6 +6,14 @@ public class targetcollider : MonoBehaviour
 {
     public static targetcollider instance;
 
+    void Start(){
+        Vector3 temp;
+        temp.x = 1.261f;
+        temp.y = 0.217f;
+        temp.z = -1.285f;
+        transform.position = new Vector3 (temp.x, temp.y, temp.z);
+    }
+
     void Awake(){
         if(instance == null){
             instance = this;
@@ -19,7 +27,7 @@ public class targetcollider : MonoBehaviour
     public void moveTarget(){
         Vector3 temp;
         temp.x = Random.Range(-2.5f, 2.5f);
-        temp.y = Random.Range(0.4f, 1f);
+        temp.y = Random.Range(0.4f, 0.8f);
         temp.z = Random.Range(-2.5f, 2.5f);
         transform.position = new Vector3 (temp.x, temp.y-6.5f, temp.z);
     }
